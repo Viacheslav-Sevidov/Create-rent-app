cfac2401cf1791be9a7b8d255d0e69d5b971c056
Viacheslav-Sevidov <ist23-v.sevidov@nubip.edu.ua>
2026-03-19 11:31:38 +0200

    Creating a master wireframe fileCreating a master wireframe file

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Оренда торгових площ</title>
+</head>
+<body>
+
+    <header>
+        <nav>
+            <a href="/">Головна</a> |
+            <a href="/about">Про проєкт</a> |
+            <a href="/spaces">Каталог площ</a>
+        </nav>
+        <hr>
+    </header>
+
+    <main>
+        <h2>Тут буде контент сторінок</h2>
+    </main>
+
+    <footer>
+        <hr>
+        <p>&copy; 2026 рік</p>
+    </footer>
+
+</body>
+</html>
